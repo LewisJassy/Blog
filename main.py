c8713a7cc2ac47c4e490f376b43ab480526edea4
LewisJassy <jassylewis70@gmail.com>
2023-11-19 19:49:39 +0300

    Added a request Module

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 from flask import Flask, render_template
+import requests
 
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
-    return render_template("index.html")
+    blog_api = "https://api.npoint.io/c790b4d5cab58020d391"
+    response = requests.get(url=blog_api)
+    data = response.json()
+    return render_template("index.html", data=data)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
